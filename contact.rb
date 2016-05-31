5d155d2a724478f24d4daa051e77f2896bcbc69d
monicamow <mow.monica@gmail.com>
2016-05-31 00:07:16 -0700

    works but cant create new csv

--- contact.rb
+++ contact.rb
@@ -1,48 +1,96 @@
 require 'csv'
 
-# Represents a person in an address book.
-# The ContactList class will work with Contact objects instead of interacting with the CSV file directly
 class Contact
 
   attr_accessor :name, :email
   
-  # Creates a new contact object
-  # @param name [String] The contact's name
-  # @param email [String] The contact's email address
   def initialize(name, email)
-    # TODO: Assign parameter values to instance variables.
+    @name = name
+    @email = email
   end
 
   # Provides functionality for managing contacts in the csv file.
   class << self
 
-    # Opens 'contacts.csv' and creates a Contact object for each line in the file (aka each contact).
-    # @return [Array<Contact>] Array of Contact objects
-    def all
-      # TODO: Return an Array of Contact instances made from the data in 'contacts.csv'.
+    def all #LIST
+      CSV.foreach('contacts.csv', :col_sep => ',') do |row|
+        contact_list = {
+          row[0] => [row[1],row[2]]
+          }
+          #row[0] => row[2] # PRINT ID SOMEHOW
+        contact_list.each do |name, info|
+          puts info[1] + ": " + name + " (" + info[0] + ")"
+        end
+      end  
     end
 
-    # Creates a new contact, adding it to the csv file, returning the new contact.
-    # @param name [String] the new contact's name
-    # @param email [String] the contact's email
-    def create(name, email)
-      # TODO: Instantiate a Contact, add its data to the 'contacts.csv' file, and return it.
+    def create
+      @contact_array = []
+
+      puts "What is the name of the contact?"
+      name_input = STDIN.gets.chomp.strip
+      @contact_array <<  name_input
+      puts "What is the email of the contact?"
+      email_input = STDIN.gets.chomp.strip
+      @contact_array <<  email_input
+
+      new_id = create_id
+      @contact_array << new_id
+      puts Contact.new(name_input, email_input)
+
+      p @contact_array
+
+      # doesn't create new file if file doesn't exist...
+      CSV.open('contacts.csv', 'a+') do |csv_object|
+          csv_object << @contact_array 
+      end
+
+    end
+
+    def create_id 
+      number_of_records = CSV.open('contacts.csv').readlines.size
+      (number_of_records + 1).to_s
     end
     
-    # Find the Contact in the 'contacts.csv' file with the matching id.
-    # @param id [Integer] the contact id
-    # @return [Contact, nil] the contact with the specified id. If no contact has the id, returns nil.
-    def find(id)
-      # TODO: Find the Contact in the 'contacts.csv' file with the matching id.
+    def find(id) #SHOW
+      @found_id = nil
+      @id = id.to_i - 1
+      CSV.open('contacts.csv', 'r') do |file|
+        file.readlines.each_with_index do |line,index|
+          if index == @id
+            @found_id = line
+          end
+        end
+      end
+      if @found_id.nil?
+        puts "not found"
+      else
+        puts @found_id
+      end
     end
     
-    # Search for contacts by either name or email.
-    # @param term [String] the name fragment or email fragment to search for
-    # @return [Array<Contact>] Array of Contact objects.
-    def search(term)
-      # TODO: Select the Contact instances from the 'contacts.csv' file whose name or email attributes contain the search term.
+    def search(term) #SEARCH
+      # THIS IS SO DISGUSTING MUST FIX THIS
+
+      @found_term = nil
+      @term = term
+      CSV.open('contacts.csv', 'r') do |file|
+        file.readlines.each do |line|
+          line.each do |field|
+            if field.downcase.include?(@term)
+              @found_term = line
+            end
+          end
+        end
+      end
+      if @found_term.nil?
+        puts "not found"
+      else
+        puts @found_term
+      end
     end
 
   end
 
 end
+
--- contact_list.rb
+++ contact_list.rb
@@ -3,6 +3,39 @@ require_relative 'contact'
 # Interfaces between a user and their contact list. Reads from and writes to standard I/O.
 class ContactList
 
+  def self.run_program
+    puts "Here is a list of available commands: \
+    \n    new    - Create a new contact \
+    \n    list   - List all contacts \
+    \n    show   - Show a contact \
+    \n    search - Search contacts"
+  end
+
+
+  ARGV << self.run_program if ARGV.empty?
+
   # TODO: Implement user interaction. This should be the only file where you use `puts` and `gets`.
 
+  def build_contact_list
+
+    case ARGV[0]
+    when "new"
+      Contact.create
+    when "list"
+      Contact.all
+    end
+    case ARGV[1]
+    when /\d/
+      Contact.find(ARGV[1])
+    when /\w/
+      Contact.search(ARGV[1])     
+    # else
+    #   puts "I don't understand."
+    end
+
+  end
+
 end
+
+test = ContactList.new
+test.build_contact_list
