905494ba82109145f5c9548c1ea249d47ff4017e
monicamow <mow.monica@gmail.com>
2016-06-08 22:30:05 -0700

    file cleanup

--- argv_example.rb
+++
@@ -1,9 +0,0 @@
-puts ARGV
-
-person = Hash.new
-
-person[:name] = ARGV[0]
-person[:age] = ARGV[1].to_i
-person[:status] = ARGV[2]
-
-puts person
--- contacts.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE contacts (
-  id serial NOT NULL PRIMARY KEY,
-  name varchar(40) NOT NULL,
-  email varchar(40) NOT NULL
-);
